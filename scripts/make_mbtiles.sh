dd29464ff18055ac3d75dadad3c54199b7713176
Tom Lee <thomas.j.lee@gmail.com>
2014-12-15 13:39:36 -0500

    script for regenerating point map of openaddresses coverage

--- scripts/make_mbtiles.sh
+++ scripts/make_mbtiles.sh
@@ -5,29 +5,31 @@ set -e
 
 ROOTDIR=`pwd`
 
+rm -rf tippecanoe build openaddresses-download
+
 sudo apt-get update -y
-sudo apt-get install -y git libsqlite3-dev protobuf-compiler libprotobuf-dev awscli nodejs
+sudo apt-get install -y git make g++ libsqlite3-dev protobuf-compiler libprotobuf-dev awscli nodejs
 git clone git@github.com:mapbox/tippecanoe.git
 cd $ROOTDIR/tippecanoe && make
 curl https://raw.githubusercontent.com/sbma44/monoxylon/master/monoxylon.js > $ROOTDIR/tippecanoe/monoxylon.js
 
 # download OpenAddresses data
-mkdir $ROOTDIR/openaddresses
-cd $ROOTDIR/openaddresses
+mkdir $ROOTDIR/openaddresses-download
+cd $ROOTDIR/openaddresses-download
 for a in $(aws s3 ls s3://data.openaddresses.io | awk -F ' ' '{print $4}' | grep csv)
 do
-    aws s3 cp --region us-east-1 s3://data.openaddresses.io/$a $ROOTDIR/openaddresses/$a
+    aws s3 cp --region us-east-1 s3://data.openaddresses.io/$a $ROOTDIR/openaddresses-download/$a
 done
 
 # concatenate address files, stripping headers
 mkdir $ROOTDIR/build
 touch $ROOTDIR/build/out.csv
-for a in $(ls $ROOTDIR/openaddresses)
+for a in $(ls $ROOTDIR/openaddresses-download)
 do
-    tail -n +1 $ROOTDIR/openaddresses/$a >> $ROOTDIR/build/out.csv
-    rm $ROOTDIR/openaddresses/$a
+    tail -n +2 $ROOTDIR/openaddresses-download/$a >> $ROOTDIR/build/out.csv
+    rm $ROOTDIR/openaddresses-download/$a
 done
 
 # build mbtiles
-cat $ROOTDIR/build/out.csv | node $ROOTDIR/tippecanoe/monoxylon.js | $ROOTDIR/tippecanoe/tippecanoe -l "openaddresses" -n "OpenAddresses `date`" -f -o $ROOTDIR/openaddresses/openaddresses.mbtiles
+cat $ROOTDIR/build/out.csv | nodejs $ROOTDIR/tippecanoe/monoxylon.js | $ROOTDIR/tippecanoe/tippecanoe -r 1.5 -l "openaddresses" -X -n "OpenAddresses `date`" -f -o $ROOTDIR/openaddresses.mbtiles
 
